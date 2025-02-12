1c11b0862a53e25c4b5c413c50b8db30fde6a546
mardev60 <makil.uspn@gmail.com>
2025-02-12 13:03:35 +0100

    Importation header dans shell

--- shell/src/App.jsx
+++ shell/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
+// The remote component provided by federation_provider
+import Header from '../../header/src/Header';
 
 const App = () => {
   return (
     <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+      <div>
+        <Header />
+      </div>
+      <h1>This is my app !</h1>
     </div>
   );
 };
